40cb52406a17948b25304b6790b9d557cd460ef4
miguel <miguel.teranj02@gmail.com>
2024-06-01 12:31:30 -0400

    contact_form: add validations, portfolio: open projects in a new tab, hero_home: contact button links to my whatsapp chat

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
-
-import react from "@astrojs/react";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import icon from "astro-icon"
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
-    enabled: false
+    enabled: false,
   },
   image: {
-    domains: ["astro.build"]
+    domains: ["astro.build"],
   },
-  integrations: [tailwind(), icon(), react()]
-});
+  integrations: [tailwind(), icon(), react()],
+})
--- src/components/UI/ContactForm.tsx
+++ src/components/UI/ContactForm.tsx
@@ -1,29 +1,20 @@
-import type { ChangeEvent, FormEvent } from "react"
 import type { FormData } from "@/src/lib/type.d.ts"
 
 import { useState, useRef } from "react"
 import emailjs from "@emailjs/browser"
 import { Icon } from "@iconify-icon/react"
+import { useForm } from "react-hook-form"
 
 export default function ContactForm() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm<FormData>()
   const form = useRef<HTMLFormElement>(null)
 
-  const [formData, setFormData] = useState<FormData>({
-    name: "",
-    email: "",
-    message: "",
-  })
-
-  const handleChange = (
-    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = e.target
-    setFormData({ ...formData, [name]: value })
-  }
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-
+  const onSubmit = handleSubmit((data: FormData) => {
     if (form.current) {
       emailjs
         .sendForm("service_u91osxk", "template_zfzl659", form.current, {
@@ -31,18 +22,20 @@ export default function ContactForm() {
         })
         .then((response: { status: number; text: string }) => {
           console.log("SUCCESS!", response.status, response.text)
-          setFormData({ name: "", email: "", message: "" })
+          console.log(data)
+
+          reset()
         })
         .catch((error: { text: string }) => {
           console.error("FAILED...", error)
         })
     }
-  }
+  })
 
   return (
     <form
       ref={form}
-      onSubmit={handleSubmit}
+      onSubmit={onSubmit}
       className="text-black flex flex-col gap-4 w-full md:max-w-[60%] mx-auto"
     >
       <p className="text-sm text-white">
@@ -51,31 +44,58 @@ export default function ContactForm() {
       </p>
 
       <input
-        onChange={handleChange}
-        value={formData.name}
+        {...register("name", {
+          required: { value: true, message: "Tu nombre es requerido" },
+          minLength: {
+            value: 2,
+            message: "Tu nombre debe tener al menos 2 caracteres",
+          },
+        })}
         type="text"
-        name="name"
         placeholder="Nombre completo"
         className="block px-4 py-2 rounded-lg bg-color-tertiary text-white border-2 border-color-primary"
       />
+      {errors.name && (
+        <span className="text-xsm text-red-500 -mt-3">
+          {errors.name.message}
+        </span>
+      )}
 
       <input
-        onChange={handleChange}
-        value={formData.email}
+        {...register("email", {
+          required: { value: true, message: "El correo es requerido" },
+          pattern: {
+            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+            message: "Formato de correo no válido",
+          },
+        })}
         type="email"
-        name="email"
         placeholder="Correo electrónico"
         className="block px-4 py-2 rounded-lg bg-color-tertiary text-white border-2 border-color-primary"
       />
+      {errors.email && (
+        <span className="text-xsm text-red-500 -mt-3">
+          {errors.email.message}
+        </span>
+      )}
 
       <textarea
-        onChange={handleChange}
-        value={formData.message}
-        name="message"
+        {...register("userMessage", {
+          required: { value: true, message: "El mensaje es requerido" },
+          minLength: {
+            value: 8,
+            message: "El mensaje debe tener al menos 8 caracteres",
+          },
+        })}
         rows={5}
         placeholder="Escribe el mensaje..."
         className="w-full block resize-none px-4 py-2 rounded-lg bg-color-tertiary text-white border-2 border-color-primary"
       ></textarea>
+      {errors.userMessage && (
+        <span className="text-xsm text-red-500 -mt-3">
+          {errors.userMessage.message}
+        </span>
+      )}
 
       <div className="flex gap-4 text-center">
         <button
